0ff616471084ea33082c6796be857dd7a1d3f6c4
Dennis Witzig <dennis@wtzg.de>
2020-05-25 22:06:30 +0200

    Add missing function prototype

--- dwm.c
+++ dwm.c
@@ -261,6 +261,7 @@ static void spawn(const Arg *arg);
 static Monitor *systraytomon(Monitor *m);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
+static void tagfocusmon(const Arg *arg);
 static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
