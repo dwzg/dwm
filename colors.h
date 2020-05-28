92a00b5857b2fb8d919b4e0bc807884fa340691f
Dennis Witzig <dennis@wtzg.de>
2020-05-28 20:17:00 +0200

    Add gruvbox colors

--- colors.h
+++ colors.h
@@ -1,4 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 
+/* personal colors */
 static const char col_orange[] = "#ffaa00";
 static const char col_gray[]   = "#231f20";
+/* gruvbox */
+static const char col_gruvbox_bg[]           = "#282828";
+static const char col_gruvbox_red[]          = "#cc241d";
+static const char col_gruvbox_green[]        = "#98971a";
+static const char col_gruvbox_yellow[]       = "#d79921";
+static const char col_gruvbox_blue[]         = "#458588";
+static const char col_gruvbox_purple[]       = "#b16286";
+static const char col_gruvbox_aqua[]         = "#689d6a";
+static const char col_gruvbox_gray[]         = "#a89984";
+static const char col_gruvbox_brightgray[]   = "#928374";
+static const char col_gruvbox_brightred[]    = "#fb4934";
+static const char col_gruvbox_brightgreen[]  = "#b8bb26";
+static const char col_gruvbox_brightyellow[] = "#fabd2f";
+static const char col_gruvbox_brightblue[]   = "#83a598";
+static const char col_gruvbox_brightpurple[] = "#d3869b";
+static const char col_gruvbox_brightaqua[]   = "#8ec07c";
+static const char col_gruvbox_fg[]           = "#ebdbb2";
